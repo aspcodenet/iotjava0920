0cb29e30d68f9eeb6fe3399bb9e63d3002fe315d
Stefan <stefan.holmberg@systementor.se>
2024-09-20 10:07:23 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        // int i = 12;
+        // ArrayList<String> list = new ArrayList<>();
+
+        // list.add("Stefan");
+        // list.add("Mats Sundin");
+        // list.add("Foppa");
+
+        // list.remove(0);  // at index
+        // list.remove("Mats Sundin");
+
+        // System.out.println(list.get(0));
+
+//		list.set(0, "Stefan Holmberg");
+
+        // for(int index = 0; index < list.size(); index++){
+        //     System.out.println(list.get(index));
+        // }
+
+//         for(String playerName : list){
+//             System.out.println(playerName);
+//         }
+
+
+
+
+
+//     //   int i = 12;
+//     //String[] players = new String[3];
+//     String[] players = {"Mats Sundin", "Peter Forsberg", "Jaromir Jagr"};
+//    //int []greatYears = {2000,2002,2008};
+//     // System.out.println(players[0]);
+//     // System.out.println(players[1]);
+//     // System.out.println(players[2]);
+
+//     players[2] = "Stefan Holmberg";
+
+//     for(int i = 0; i < players.length;i++){
+//         System.out.println(players[i]);
+//     }
+
+    //players.add("Pavel Bure")
+
+//     players[2] = "Stefan";
+//     System.out.println(players);
+
+//         // Går det inte att klumpa ihop                ARRAYER
+//             // en som heter minaBarn - 3 fack
+//         String barn1 = "Fanny";
+//         String barn2 = "Josefine";
+//         String barn3 = "Oliver";
+   
+//         System.out.println("Stefans barn är");
+//         System.out.println(barn1);
+//         System.out.println(barn2);
+//         System.out.println(barn3);
+        
+//    //Går det att göra så att man "1. Skapa nytt barn"      // LISTOR
+
+//         System.out.println("Hello, World!");
+    }
+}
